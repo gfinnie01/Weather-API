b5d9f5adc0b74f2d80943e29479c3249cb29cb6f
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-31 21:43:18 -0400

    got my api key and should have most of my query selecters

---
+++ public/index.js
@@ -0,0 +1,6 @@
+var appid = "91f7010a8e5fcd16dbb7833271ef9718";
+var currentE1 = document.querySelector('#current');
+var searchForm = document.querySelector('#search-Form')
+var searchE1 = document.querySelector('#search')
+var citySearch = document.querySelector("#searched-Cities")
+var fiveDayE1 = document.querySelector('#fiveDay')
